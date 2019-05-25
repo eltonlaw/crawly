ec3483d69faf1d473b55086364811d3348a2ac9c
Elton Law <eltonlaw296@gmail.com>
2019-05-25 00:19:23 -0400

    add basic GET call

--- src/crawly/core.clj
+++ src/crawly/core.clj
@@ -1,6 +1,10 @@
-(ns crawly.core)
+(ns crawly.core
+  (:require [clj-http.client :as client]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn GET
+  [url]
+  (let [response (client/get url)]
+    (if (= (:status response) 200)
+      (:body response))))
+
+
