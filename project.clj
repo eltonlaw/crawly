147ecd912716ee059f7cb27abf4ac9f082750133
Elton Law <eltonlaw296@gmail.com>
2019-05-25 00:14:54 -0400

    update dependencies for crawly.dev

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject crawly "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Web crawling framework for Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :resource-paths #{"src" "dev"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.9.1"]
+                 [hiccup "1.0.5"]
+                 [http-kit "2.3.0"]]
+  :repl-options {:init-ns crawly.dev})
