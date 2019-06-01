4328eac410146658a1c72292fe3f1072137295aa
Elton Law <eltonlaw296@gmail.com>
2019-06-01 15:12:27 -0400

    remove extra start server
    Until we have another handler, this won't work even if we pass in a
    different port because of the way that servers are being saved (via
    assoc into map using handler fn as key)

--- test/crawly/dev_test.clj
+++ test/crawly/dev_test.clj
@@ -3,25 +3,27 @@
             [crawly.dev :as dev]
             [crawly.test-handlers :as test-handlers]))
 
+(def not-empty? (complement empty?))
+
 (deftest start-stop-server
   (testing "At each step, check that state of `servers` atom is as expected"
     (testing "Start server & stop by key"
       (is (empty? @dev/servers))
       (dev/start-server! test-handlers/basic)
-      (is ((complement empty?) @dev/servers))
+      (is (not-empty? @dev/servers))
       (dev/stop-servers! test-handlers/basic)
       (is (empty? @dev/servers)))
     (testing "Start servers & stop all by passing no argument"
       (is (empty? @dev/servers))
       (dev/start-server! test-handlers/basic)
-      (dev/start-server! test-handlers/basic {:port 3001 :join? false})
-      (is ((complement empty?) @dev/servers))
+      (is (not-empty? @dev/servers))
       (dev/stop-servers!)
       (is (empty? @dev/servers)))))
 
 (deftest with-server
   (testing "make sure that server only exists within context"
+    (println "@dev/servers" @dev/servers)
     (is (empty? @dev/servers))
     (dev/with-server [server test-handlers/basic]
-      (is ((complement empty?) @dev/servers)))
+      (is (not-empty? @dev/servers)))
     (is (empty? @dev/servers))))
