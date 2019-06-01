7b068938b9a0ac8b56efc73c9c1e034c13f91f94
Elton Law <eltonlaw296@gmail.com>
2019-06-01 15:26:19 -0400

    prevent hanging instances by adding check for handler in atom

--- dev/crawly/dev.clj
+++ dev/crawly/dev.clj
@@ -9,13 +9,6 @@
 (def url "http://localhost:3000") ;; Default url
 (def servers (atom {}))
 
-(defn start-server!
-  ([handler] (start-server! handler {:port 3000 :join? false}))
-  ([handler metadata]
-   (swap! servers assoc handler (run-server handler metadata))
-   (when (some? (get @servers handler))
-     (println "...successfully started server -" handler))))
-
 (defn stop-servers!
   [& handlers]
   (doseq [handler (or (seq handlers)
@@ -24,6 +17,16 @@
       (server)
       (swap! servers dissoc handler)
       (println "...successfully stopping server -" handler))))
+
+(defn start-server!
+  ([handler] (start-server! handler {:port 3000 :join? false}))
+  ([handler metadata]
+   (when (get @servers handler)
+     (println "...handler being replicated. To prevent hanging instance deleting old version first... ")
+     (stop-servers! handler))
+   (swap! servers assoc handler (run-server handler metadata))
+   (when (get @servers handler)
+     (println "...successfully started server -" handler))))
 
 (defmacro with-server [bindings body]
   (let [[server handler] bindings]
