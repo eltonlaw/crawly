05e5f442a151f279f6f20018d592cc579bf431b8
Elton Law <eltonlaw296@gmail.com>
2019-06-02 03:10:43 -0400

    unit tests for cached responses

--- test/crawly/core_test.clj
+++ test/crawly/core_test.clj
@@ -1,13 +1,14 @@
 (ns crawly.core-test
   (:require [clojure.test :refer [deftest is testing]]
-            [crawly.dev :as dev]
-            [crawly.test-handlers :as test-handlers]
-            [crawly.core :as core]))
+            [crawly.cache :as cache]
+            [crawly.core :as core] [crawly.dev :as dev]
+            [crawly.test-handlers :as test-handlers]))
 
 (deftest set-cache-level!
   (testing "Valid level passed in"
     (is (= (core/set-cache-level! :aggressive)
-           :aggressive)))
+           :aggressive))
+    (core/set-cache-level! :none))
   (testing "Invalid level passed in"
     (try
       (core/set-cache-level! 5)
@@ -16,6 +17,16 @@
                {:causes {:invalid-level 5}}))))))
 
 (deftest GET
-  (dev/with-server [server test-handlers/basic]
-    (is (= "<!DOCTYPE html>\n<html><body><div>some content</div></body></html>"
-           (core/GET "http://localhost:3000")))))
+  (testing "basic GET request returns body"
+    (dev/with-server [server test-handlers/basic]
+      (is (= "<!DOCTYPE html>\n<html><body><div>some content</div></body></html>"
+             (-> (core/GET "http://localhost:3000") :body)))))
+  (testing "GET is cached"
+    (core/set-cache-level! :aggressive)
+    (dev/with-server [server test-handlers/basic]
+      (cache/delete-dir-files @cache/path)
+      (let [tmpdir-filecount #(or (count (.listFiles (clojure.java.io/as-file @cache/path))) 0)]
+        (is (= 0 (tmpdir-filecount)))
+        (core/GET "http://localhost:3000")
+        (is (= 1 (tmpdir-filecount)))))
+    (core/set-cache-level! :none)))
