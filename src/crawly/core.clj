f548e53fc0a206a79f9048d4b8c5934116e79c17
Elton Law <eltonlaw296@gmail.com>
2020-06-22 06:04:05 -0400

    move consumption of jobs into global process

--- src/crawly/core.clj
+++ src/crawly/core.clj
@@ -1,28 +1,29 @@
 (ns crawly.core
   (:require
-    [clojure.core.async :refer [go >! <! chan]]
+    [clojure.core.async :refer [go go-loop >! <! chan]]
     [clojure.java.io :as io]
     [clojure.string :as str]
     [org.httpkit.client :as client]
     [taoensso.timbre :as timbre]
     [taoensso.timbre.appenders.core :as appenders]))
 
-;; Global state shared between scraping threads
-(def job-count (atom 0))
-(def response-ch (chan))
-
 ;; Path string to file containing requests
 (def ^:dynamic *requests-log-file*
-  (or (System/getenv "CRAWLY_REQUESTS_LOG") "crawly.requests.log")
+  (or (System/getenv "CRAWLY_REQUESTS_LOG") "crawly.requests.log"))
 
 ;; Path string to file containing all log messages
 (def ^:dynamic *log-file*
-  (or (System/getenv "CRAWLY_LOG") "crawly.log")
+  (or (System/getenv "CRAWLY_LOG") "crawly.log"))
 
 ;; Logging initialization
 (timbre/merge-config!
   {:level :debug
    :appenders {:spit (appenders/spit-appender {:fname *log-file*})}})
+
+;; Global state shared between scraping threads
+(def job-count (atom 0))
+(def jobs (atom {}))
+(def response-ch (chan))
 
 (defn GET
   "Run GET on url in thread, putting onto response channel when done"
@@ -30,6 +31,7 @@
   ([url {:keys [timeout post-fn] :or {timeout 2000
                                       post-fn identity}}]
    (let [id (swap! job-count inc)]
+     ;; FIXME: This needs to be done all at once, need to put into a go-loop i think
      (spit *requests-log-file* (format "GET,%d,%s\n" id url) :append true)
      (timbre/infof "[%d] GET %s" id url)
      (go
@@ -40,3 +42,13 @@
                         {:status 408})]
          (>! response-ch {:id id :response response})))
      id)))
+
+(defn process-jobs-worker!
+  "Starts a new thread which constantly takes responses off the response channel
+  and assocs them to the global map of finished jobs. Running multiple versions
+  of this shouldn't cause any issues."
+  []
+  (go-loop []
+    (let [response (<! response-ch)]
+      (swap! jobs-lookup assoc (:id response) response))
+    (recur)))
--- test/crawly/core_test.clj
+++ test/crawly/core_test.clj
@@ -3,6 +3,9 @@
             [clojure.core.async :refer [<!!]]
             [clojure.test :refer [deftest is]]))
 
+(core/process-jobs-worker!)
+
 (deftest ^:integration GET
   (let [job-id-1 (core/GET "http://localhost:9500/#/")]
-    (is (= 200 (get-in (<!! core/response-ch) [:response :status])))))
+    (Thread/sleep 1000) ;; FIXME: Dirty hack
+    (is (= 200 (get-in @core/jobs [job-id-1 :response :status])))))
